5a3a2f0575da8e3c2b003b7946257db5c58021b3
kn52 <ashish52922@gmail.com>
2020-02-17 16:14:21 +0530

    ADDED:First name validation

--- src/main/java/validation/UserRegistration.java
+++ src/main/java/validation/UserRegistration.java
@@ -1,8 +1,10 @@
 package validation;
 
 public class UserRegistration {
-
+    private String NAME_REGEX="^[A-Z]{1}[a-z]{2,}$";
     public static void main(String[] args) {
         System.out.println("Welcome to user registration problem");
     }
+    public boolean validateFirstName(String fname) { return fname.matches(NAME_REGEX); }
+
 }
---
+++ src/test/java/com/user/test/UserRegistrationTest.java
@@ -0,0 +1,22 @@
+package com.user.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import validation.UserRegistration;
+
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration user=new UserRegistration();
+        boolean result = user.validateFirstName("Ashish");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+        UserRegistration user=new UserRegistration();
+        boolean result = user.validateFirstName("ashish");
+        Assert.assertEquals(false,result);
+    }
+
+}
